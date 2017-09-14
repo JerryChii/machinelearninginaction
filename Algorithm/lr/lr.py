e236e5873f3c344c7721c36632b8659a30a34f35
JerryChii <1625742718@qq.com>
2017-09-15 00:05:12 +0800

    implement lr algorithm

--- Algorithm/lr/lr.py
+++ Algorithm/lr/lr.py
@@ -15,5 +15,27 @@ def load_lr_data():
 def sigmoid(z):
     return 1.0/(1.0+np.exp(-z))
 
+def graDecent(dataMat,labelMat):
+    m,n = dataMat.shape
+    X0 = np.ones((m,1)).astype(dataMat.dtype)
+    dataMat = np.mat(np.column_stack((X0, dataMat)))
+    labelMat = np.mat(labelMat).T
+
+    theta = np.mat(np.ones((dataMat.shape[1],1)))
+    lambda_val = 0.1
+    alpha = 0.001
+    maxCycles = 500
+
+    for k in range(maxCycles):
+        h = sigmoid(dataMat * theta)
+        err = (h - labelMat)
+        theta = theta*(1-alpha*lambda_val/m) - alpha * dataMat.T * err
+
+    return theta
+
+
+dataMat, labelMat = load_lr_data()
+
+print graDecent(dataMat, labelMat)
 
 # print sigmoid(np.array([0,0]))
